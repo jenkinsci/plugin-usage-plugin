038555928724c3dc2bda5c7c4232878ef26f14b6
Filipe Roque <froque@premium-minds.com>
2019-12-20 12:28:10 +0000

    Handle plugins as optional dependencies correctly
    https://wiki.jenkins.io/display/JENKINS/Tips+for+optional+dependencies

--- src/main/java/org/jenkinsci/plugins/pluginusage/analyzer/MavenJobAnalyzer.java
+++ src/main/java/org/jenkinsci/plugins/pluginusage/analyzer/MavenJobAnalyzer.java
@@ -3,6 +3,7 @@ package org.jenkinsci.plugins.pluginusage.analyzer;
 import hudson.PluginWrapper;
 import hudson.maven.MavenModuleSet;
 import hudson.model.Job;
+import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.pluginusage.JobsPerPlugin;
 
 import java.util.Map;
@@ -12,16 +13,18 @@ public class MavenJobAnalyzer  extends JobAnalyzer {
     @Override
     protected void doJobAnalyze(Job item, Map<PluginWrapper, JobsPerPlugin> mapJobsPerPlugin) {
         super.doJobAnalyze(null, mapJobsPerPlugin);
-        if (item instanceof MavenModuleSet) {
-            PluginWrapper usedPlugin = getUsedPlugin(MavenModuleSet.DescriptorImpl.class);
-            if (usedPlugin != null) {
-                JobsPerPlugin jobsPerPlugin = mapJobsPerPlugin.get(usedPlugin);
-                if (jobsPerPlugin != null) {
-                    jobsPerPlugin.addProject(item);
-                } else {
-                    JobsPerPlugin jobsPerPlugin2 = new JobsPerPlugin(usedPlugin);
-                    jobsPerPlugin2.addProject(item);
-                    mapJobsPerPlugin.put(usedPlugin, jobsPerPlugin2);
+        if (Jenkins.get().getPlugin("maven-plugin") != null){
+            if (item instanceof MavenModuleSet) {
+                PluginWrapper usedPlugin = getUsedPlugin(MavenModuleSet.DescriptorImpl.class);
+                if (usedPlugin != null) {
+                    JobsPerPlugin jobsPerPlugin = mapJobsPerPlugin.get(usedPlugin);
+                    if (jobsPerPlugin != null) {
+                        jobsPerPlugin.addProject(item);
+                    } else {
+                        JobsPerPlugin jobsPerPlugin2 = new JobsPerPlugin(usedPlugin);
+                        jobsPerPlugin2.addProject(item);
+                        mapJobsPerPlugin.put(usedPlugin, jobsPerPlugin2);
+                    }
                 }
             }
         }
