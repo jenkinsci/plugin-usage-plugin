d6f2f87be7cb79d386bb312458b41be26735ad8e
WilliamMarsVolant <William.Mars@Volantco.com>
2020-08-07 08:25:19 -0400

    Refactored permission requirements

--- src/main/java/org/jenkinsci/plugins/pluginusage/PluginUsageView.java
+++ src/main/java/org/jenkinsci/plugins/pluginusage/PluginUsageView.java
@@ -13,33 +13,21 @@ import org.jvnet.localizer.ResourceBundleHolder;
 public class PluginUsageView implements RootAction {
 
 
-	private static final ResourceBundleHolder HOLDER = new ResourceBundleHolder(PluginUsageView.class);
-
-	private static final org.jvnet.localizer.Localizable DESCRIPTION =
-			new org.jvnet.localizer.Localizable(HOLDER,"PluginUsageView.ViewPermission");
-
-	private static final PermissionGroup PERMISSIONS = new PermissionGroup(
-			PluginUsageView.class, DESCRIPTION);
-
-	private static final Permission VIEW = new Permission(PERMISSIONS, "PluginsUsageView.ViewPermission",
-			DESCRIPTION, Jenkins.ADMINISTER, PermissionScope.JENKINS);
-
 	public String getDisplayName() {
 		return "Plugin Usage";
 	}
 
 	public String getIconFileName() {
-		return (Jenkins.get().hasPermission(VIEW)) ? "plugin.png" : null;
+		return (Jenkins.get().hasPermission(Jenkins.ADMINISTER)) ? "plugin.png" : null;
 	}
 
 	public String getUrlName() {
-		return (Jenkins.get().hasPermission(VIEW)) ? "pluginusage" : null;
+		return (Jenkins.get().hasPermission(Jenkins.ADMINISTER)) ? "pluginusage" : null;
 	}
 	
 	public PluginUsageModel getData() {
-		if (Jenkins.get().hasPermission(VIEW)) {
-			PluginUsageModel pluginUsageModel = new PluginUsageModel();
-			return pluginUsageModel;
+		if (Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+			return new PluginUsageModel();
 		}
 		return null;
 	}
