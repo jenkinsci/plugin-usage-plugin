ba860a29bc72e434993ca08a1a04e3e2c0535f3f
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-06-28 12:42:15 +0100

    read access validation

--- src/main/java/org/jenkinsci/plugins/pluginusage/PluginUsageView.java
+++ src/main/java/org/jenkinsci/plugins/pluginusage/PluginUsageView.java
@@ -3,28 +3,35 @@ package org.jenkinsci.plugins.pluginusage;
 import hudson.Extension;
 import hudson.model.Api;
 import hudson.model.RootAction;
+import jenkins.model.Jenkins;
 
 @Extension
 public class PluginUsageView implements RootAction{
-	
+
 	public String getDisplayName() {
 		return "Plugin Usage";
 	}
 
 	public String getIconFileName() {
-		return "plugin.png";
+		return (Jenkins.get().hasPermission(Jenkins.READ)) ? "plugin.png" : null;
 	}
 
 	public String getUrlName() {
-		return "pluginusage";
+		return (Jenkins.get().hasPermission(Jenkins.READ)) ? "pluginusage" : null;
 	}
-	
+
 	public PluginUsageModel getData() {
-		PluginUsageModel pluginUsageModel = new PluginUsageModel();
-		return pluginUsageModel;
+		if (Jenkins.get().hasPermission(Jenkins.READ)) {
+			PluginUsageModel pluginUsageModel = new PluginUsageModel();
+			return pluginUsageModel;
+		}
+		return null;
 	}
 
 	public Api getApi() {
-		return new Api(getData());
+		if (Jenkins.get().hasPermission(Jenkins.READ)) {
+			return new Api(getData());
+		}
+		return null;
 	}
 }
--- src/test/java/org/jenkinsci/plugins/pluginusage/JobAnalyzerTest.java
+++ src/test/java/org/jenkinsci/plugins/pluginusage/JobAnalyzerTest.java
@@ -2,22 +2,52 @@ package org.jenkinsci.plugins.pluginusage;
 
 import static org.junit.Assert.*;
 
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.model.FreeStyleProject;
-
+import jenkins.model.Jenkins;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.MockAuthorizationStrategy;
+
+import java.net.HttpURLConnection;
 
 public class JobAnalyzerTest {
 
 	@Rule public JenkinsRule j = new JenkinsRule();
-	
-	@Test 
+
+	@Test
 	public void first() throws Exception {
 	    FreeStyleProject project = j.createFreeStyleProject();
 	    PluginUsageView pluginUsageView = new PluginUsageView();
 	    PluginUsageModel data = pluginUsageView.getData();
-	    assertEquals(1,data.getNumberOfJobs());    
-	  }
+	    assertEquals(1,data.getNumberOfJobs());
+	}
 
+    @Test
+    public void onlyReadCanReadPluginUsage() throws Exception {
+        final String READONLY = "readonly";
+        final String ANONYMOUS = "anonymous";
+
+        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
+        j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy()
+                // Read access
+                .grant(Jenkins.READ).everywhere().to(READONLY)
+        );
+
+        JenkinsRule.WebClient wc = j.createWebClient()
+                .withThrowExceptionOnFailingStatusCode(false);
+
+        { // user can access it
+            wc.login(READONLY);
+            HtmlPage page = wc.goTo("pluginusage/");
+            assertEquals(HttpURLConnection.HTTP_OK, page.getWebResponse().getStatusCode());
+        }
+
+        { // anonymous cannot see it
+            wc.login(ANONYMOUS);
+            HtmlPage page = wc.goTo("pluginusage/");
+            assertEquals(HttpURLConnection.HTTP_FORBIDDEN, page.getWebResponse().getStatusCode());
+        }
+    }
 }
