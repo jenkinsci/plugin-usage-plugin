12601fc8fafa90c0c80ab59da7af95dfc94f7e41
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-06-28 13:17:54 +0100

    Add specific permissions for the plugin

--- src/main/java/org/jenkinsci/plugins/pluginusage/PluginUsageView.java
+++ src/main/java/org/jenkinsci/plugins/pluginusage/PluginUsageView.java
@@ -3,35 +3,46 @@ package org.jenkinsci.plugins.pluginusage;
 import hudson.Extension;
 import hudson.model.Api;
 import hudson.model.RootAction;
+import hudson.security.Permission;
+import hudson.security.PermissionGroup;
+import hudson.security.PermissionScope;
 import jenkins.model.Jenkins;
 
 @Extension
 public class PluginUsageView implements RootAction{
+    /**
+     * Permission group for PluginUsageView related permissions.
+     */
+    public static final PermissionGroup PERMISSIONS =
+        new PermissionGroup(PluginUsageView.class, Messages._PluginUsageView_PermissionGroup());
+	/**
+     * Permission to get the plugin view usage.
+     */
+    public static final Permission PLUGIN_VIEW = new Permission(PERMISSIONS,
+        "PluginView", Messages._PluginUsageView_PluginViewPermission_Description(), Jenkins.ADMINISTER, PermissionScope.JENKINS);
 
 	public String getDisplayName() {
 		return "Plugin Usage";
 	}
 
 	public String getIconFileName() {
-		return (Jenkins.get().hasPermission(Jenkins.READ)) ? "plugin.png" : null;
+        Jenkins.getInstance().checkPermission(PluginUsageView.PLUGIN_VIEW);
+		return "plugin.png";
 	}
 
 	public String getUrlName() {
-		return (Jenkins.get().hasPermission(Jenkins.READ)) ? "pluginusage" : null;
+        Jenkins.getInstance().checkPermission(PluginUsageView.PLUGIN_VIEW);
+		return "pluginusage";
 	}
 
 	public PluginUsageModel getData() {
-		if (Jenkins.get().hasPermission(Jenkins.READ)) {
-			PluginUsageModel pluginUsageModel = new PluginUsageModel();
-			return pluginUsageModel;
-		}
-		return null;
+        Jenkins.getInstance().checkPermission(PluginUsageView.PLUGIN_VIEW);
+		PluginUsageModel pluginUsageModel = new PluginUsageModel();
+		return pluginUsageModel;
 	}
 
 	public Api getApi() {
-		if (Jenkins.get().hasPermission(Jenkins.READ)) {
-			return new Api(getData());
-		}
-		return null;
+		Jenkins.getInstance().checkPermission(PluginUsageView.PLUGIN_VIEW);
+		return new Api(getData());
 	}
 }
--- src/test/java/org/jenkinsci/plugins/pluginusage/JobAnalyzerTest.java
+++ src/test/java/org/jenkinsci/plugins/pluginusage/JobAnalyzerTest.java
@@ -14,32 +14,43 @@ import java.net.HttpURLConnection;
 
 public class JobAnalyzerTest {
 
-	@Rule public JenkinsRule j = new JenkinsRule();
-
-	@Test
-	public void first() throws Exception {
-	    FreeStyleProject project = j.createFreeStyleProject();
-	    PluginUsageView pluginUsageView = new PluginUsageView();
-	    PluginUsageModel data = pluginUsageView.getData();
-	    assertEquals(1,data.getNumberOfJobs());
-	}
+    @Rule public JenkinsRule j = new JenkinsRule();
 
     @Test
-    public void onlyReadCanReadPluginUsage() throws Exception {
-        final String READONLY = "readonly";
+    public void first() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        PluginUsageView pluginUsageView = new PluginUsageView();
+        PluginUsageModel data = pluginUsageView.getData();
+        assertEquals(1,data.getNumberOfJobs());
+    }
+
+    @Test
+    public void onlyReadOrAdminCanReadPluginUsage() throws Exception {
+        final String PLUGIN_VIEW = "plugin-view-user";
         final String ANONYMOUS = "anonymous";
+        final String ADMIN = "admin";
 
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
         j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy()
+                // Plugin view access
+                .grant(PluginUsageView.PLUGIN_VIEW).everywhere().to(PLUGIN_VIEW)
                 // Read access
-                .grant(Jenkins.READ).everywhere().to(READONLY)
+                .grant(Jenkins.READ).everywhere().to(PLUGIN_VIEW)
+                // Admin access
+                .grant(Jenkins.ADMINISTER).everywhere().to(ADMIN)
         );
 
         JenkinsRule.WebClient wc = j.createWebClient()
                 .withThrowExceptionOnFailingStatusCode(false);
 
         { // user can access it
-            wc.login(READONLY);
+            wc.login(PLUGIN_VIEW);
+            HtmlPage page = wc.goTo("pluginusage/");
+            assertEquals(HttpURLConnection.HTTP_OK, page.getWebResponse().getStatusCode());
+        }
+
+        { // admin can access it
+            wc.login(ADMIN);
             HtmlPage page = wc.goTo("pluginusage/");
             assertEquals(HttpURLConnection.HTTP_OK, page.getWebResponse().getStatusCode());
         }
